bb099984ee2b1b6f67650e645a906273e2e026ed
Wesley0010012 <wglaurindo33@gmail.com>
2024-03-30 10:10:56 -0300

    feat: add components properties attributes and values using parent attributes

--- template/src/components/CardNews.js
+++ template/src/components/CardNews.js
@@ -1,4 +1,7 @@
 class CardNews extends HTMLElement {
+    BASE_URL = "http://localhost:5500/"
+    BASE_IMAGE_URL = this.BASE_URL + "assets/img/"
+    
     constructor() {
         super()
 
@@ -16,8 +19,14 @@ class CardNews extends HTMLElement {
         cardLeft.setAttribute('class', 'card_left')
 
         const autor = document.createElement('span')
+        autor.textContent = "By " + (this.getAttribute('autor') ?? "Anonymous") 
+        
         const linkTitle = document.createElement('a')
+        linkTitle.textContent = (this.getAttribute('title'));
+        linkTitle.href = this.BASE_URL + this.getAttribute('link-url')
+
         const newsContent = document.createElement('p')
+        newsContent.textContent = this.getAttribute('content')
 
         cardLeft.appendChild(autor)
         cardLeft.appendChild(linkTitle)
@@ -26,7 +35,8 @@ class CardNews extends HTMLElement {
         const cardRight = document.createElement('div')
         cardRight.setAttribute('class', 'card_right')
 
-        const image = document.createElement('image')
+        const image = document.createElement('img')
+        image.src = this.BASE_IMAGE_URL + this.getAttribute('image-url')
         
         cardRight.appendChild(image)
 
